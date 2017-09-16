bf386327677789f286d5957aa94d5c7cf4e29929
nhatnv <nguyenvannhat152@gmail.com>
2017-09-16 18:28:27 +0700

    fix

--- src/config/page_token.js
+++ src/config/page_token.js
@@ -1,4 +1,4 @@
-export default {
-    page_token: "REPLACE WITH YOUR PAGE TOKEN",
+module.exports = {
+    page_token: "EAABwWiQYzzkBAMdWZAVIZAnJmyc1kC8A826Mc6ZBPr2aUZA3nCmp6Y3mlwTrdUPWUO3o0EPdldG1oMPIHUncc23oIZBrjsNmpQXHRtnV6GRs492DiS6ijZBhUFUiljzVMWpDTKdAkGTKBnkl74ZAwW7WpUcaWGqjkvMdSykZAcPHowZDZD",
     verify_token: "verify_token"
-}
+};
