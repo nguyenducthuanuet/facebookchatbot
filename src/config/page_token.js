1fe9dd7edd7603526451be2ea6190206eb25cb13
nhatnv <nguyenvannhat152@gmail.com>
2017-09-17 17:17:46 +0700

    remove

--- src/config/page_token.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    page_token: "EAABwWiQYzzkBAIq6ZChjN3oHik7NYm9BYIoAkZCyqSFgfOZCJbhuusFDyQKZC6K3J2ix0DegFblZCUkEP3sLf2aO1CMeoCUZAi2ywo88XAM00XVCjLDGfvl606IDrKaPea4f2Q6GYGTXpMtEDxjYrO8SAF20Hj1HljGXZCY2E5dZAwZDZD",
-    verify_token: "verify_token"
-};
